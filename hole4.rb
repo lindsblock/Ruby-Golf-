3f87f29e4ffb477387dd0dd3f3d342c6267b0488
Lindsay Block <lindsay.block05@gmail.com>
2018-02-19 17:26:04 -0700

    starting hole 4

---
+++ hole4.rb
@@ -0,0 +1,13 @@
+#The method takes in a number and a max value 
+#then finds all multiples of the number up to the max value.
+
+def start
+  puts" Give me a number"
+  input = gets.to_i
+  puts "Give me a higher number"
+  input2 = gets.to_i
+end
+start
+#find multiples of input
+#stops at input2
+% input = 0 
