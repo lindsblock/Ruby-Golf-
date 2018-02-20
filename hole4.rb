3a392afe74bc5e63251dae44d6cc32c7131aa03d
Lindsay Block <lindsay.block05@gmail.com>
2018-02-19 21:39:14 -0700

    working out logic for hole 4

--- hole4.rb
+++ hole4.rb
@@ -6,8 +6,19 @@ def start
   input = gets.to_i
   puts "Give me a higher number"
   input2 = gets.to_i
+  
+  array= []
+  i = 0
+  while i < input2 #lists all numbers less than the max number and pushes them into an array starting at 0 
+    i +=1
+    array << i
+  end 
+  
+  array.each do |x| #for each thing in the array check to see if it is a multiple of the input 
+    if x % input == 0 # if it is a multiple then the molulus would be 0 
+      puts x 
+    else 
+    end 
+  end 
 end
 start
-#find multiples of input
-#stops at input2
-% input = 0 
